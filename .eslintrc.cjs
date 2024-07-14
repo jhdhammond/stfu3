0329c785f67ca0a5f037022e16ffdd1f311615f1
Jack Hammond <jack.hammond@gmail.com>
2024-07-14 19:20:26 +0100

    xx

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
+  "root": true,
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
     "project": true
@@ -7,36 +8,8 @@ const config = {
   "plugins": [
     "@typescript-eslint"
   ],
-  "extends": [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked"
-  ],
-  "rules": {
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        "prefer": "type-imports",
-        "fixStyle": "inline-type-imports"
-      }
-    ],
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        "argsIgnorePattern": "^_"
-      }
-    ],
-    "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/no-misused-promises": [
-      "error",
-      {
-        "checksVoidReturn": {
-          "attributes": false
-        }
-      }
-    ]
-  }
-}
+  "extends": [],
+  "rules": {}
+};
+
 module.exports = config;
